7da912500f671375a48daa2c97444fb07be3a504
Mark Alvaro <markaalvaro@gmail.com>
2020-12-07 20:45:04 -0500

    Day 6 commit part 2.

--- day6.py
+++ day6.py
@@ -8,6 +8,23 @@ def count_affirmatives_per_group(groups):
     
     return count
 
+def to_set(str):
+    return set(str)
+
+def count_full_group_affirmatives(groups):
+    count = 0
+
+    for group in groups:
+        answers = re.split(r'\s+', group)
+        affirmatives = set(answers[0])
+        for i in range(1, len(answers)):
+            affirmatives = affirmatives.intersection(answers[i])
+        count += len(affirmatives)
+    
+    return count
+
+
 with open('day6_input.txt', 'r') as file:
     groups = file.read().split('\n\n')
     print(count_affirmatives_per_group(groups))
+    print(count_full_group_affirmatives(groups))
